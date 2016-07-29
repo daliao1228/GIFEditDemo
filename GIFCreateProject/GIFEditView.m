a1bd84db51289961ef2dfd799642c5381ae32701
liushuoyang <liushuoyang@changba.com>
2016-07-29 18:57:10 +0800

    添加scaleButton手势

--- GIFCreateProject/GIFEditView.m
+++ GIFCreateProject/GIFEditView.m
@@ -12,18 +12,19 @@
 #define MAXSCALE ([UIScreen mainScreen].bounds.size.width - 10) / 100
 #define MINSCALE 0.25f
 
-@interface GIFEditView ()
+@interface GIFEditView () <UIGestureRecognizerDelegate>
 
 @property (strong, nonatomic) IBOutlet GIFView *gifImageView;
 @property (strong, nonatomic) IBOutlet UIButton *deleteButton;
 @property (strong, nonatomic) IBOutlet UIButton *rotateButton;
-@property (strong, nonatomic) IBOutlet UIButton *scaleButton;
+@property (strong, nonatomic) IBOutlet UIButton *rotateAndScaleButton;
 @property (strong, nonatomic) UIPanGestureRecognizer *panRecognizer;
 @property (strong, nonatomic) UIPanGestureRecognizer *rotateButtonPanRecognizer;
 @property (strong, nonatomic) UIPinchGestureRecognizer *pinchRecognizer;
 @property (strong, nonatomic) UIRotationGestureRecognizer *rotationRecognizer;
 @property (assign, nonatomic) CGFloat towPointPinchDistance;
 @property (assign, nonatomic) CGPoint lastPointPosition;
+@property (assign, nonatomic) CGFloat originCenterDistance;
 
 @end
 
@@ -54,25 +55,32 @@
 - (void)initSomeGestures {
     if (!self.panRecognizer) {
         self.panRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+        self.panRecognizer.delegate = self;
         [self addGestureRecognizer:self.panRecognizer];
     }
     if (!self.pinchRecognizer) {
         self.pinchRecognizer = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinch:)];
+        self.panRecognizer.delegate = self;
         [self addGestureRecognizer:self.pinchRecognizer];
     }
     if (!self.rotationRecognizer) {
         self.rotationRecognizer = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotate:)];
+        self.panRecognizer.delegate = self;
         [self addGestureRecognizer:self.rotationRecognizer];
     }
+    
     if (!self.rotateButtonPanRecognizer) {
         self.rotateButtonPanRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotatePan:)];
-        [self.rotateButton addGestureRecognizer:self.rotateButtonPanRecognizer];
+        self.panRecognizer.delegate = self;
+        [self.rotateAndScaleButton addGestureRecognizer:self.rotateButtonPanRecognizer];
     }
 }
 
 - (void)initGIFViews {
     [self.gifImageView setModels:self.model];
     [self.gifImageView startAnimating];
+    CGPoint rightBottomPoint = CGPointMake(self.center.x + self.bounds.size.width / 2, self.center.y + self.bounds.size.height / 2);
+    self.originCenterDistance = [self caculateTwoPointDistance:self.center secondPoint:rightBottomPoint];
 }
 
 - (void)setModel:(GIFModel *)model {
@@ -84,8 +92,8 @@
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
     if (CGRectContainsPoint(self.deleteButton.frame, point)) {
         return self.deleteButton;
-    } else if (CGRectContainsPoint(self.scaleButton.frame, point)) {
-        return self.scaleButton;
+    } else if (CGRectContainsPoint(self.rotateAndScaleButton.frame, point)) {
+        return self.rotateAndScaleButton;
     }
     return [super hitTest:point withEvent:event];
 }
@@ -139,14 +147,34 @@
         if (self.rotateButtonPanRecognizer.state == UIGestureRecognizerStateBegan) {
             self.lastPointPosition = currentTouchPosition;
         } else if (self.rotateButtonPanRecognizer.state == UIGestureRecognizerStateChanged) {
+            
             CGPoint center = self.center;
+            CGRect bounds = self.bounds;
+            
             CGFloat rotation = [self angleWithPoint:center pointA:self.lastPointPosition pointB:currentTouchPosition];
-            self.lastPointPosition = currentTouchPosition;
             self.transform = CGAffineTransformRotate(self.transform, rotation);
+            self.lastPointPosition = currentTouchPosition;
+            
+            CGFloat distance = [self caculateTwoPointDistance:center secondPoint:currentTouchPosition];
+            CGFloat scale = distance / self.originCenterDistance;
+            bounds.size.width = 100 * scale;
+            bounds.size.height = 100 * scale;
+            
+            self.bounds = bounds;
         }
     }
 }
 
+#pragma -mark UIGestureRecognizerDelegate
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    if ((gestureRecognizer == self.pinchRecognizer && otherGestureRecognizer == self.rotationRecognizer) ||
+        (gestureRecognizer == self.rotationRecognizer && otherGestureRecognizer == self.pinchRecognizer)) {
+        return YES;
+    }
+    return NO;
+}
+
+#pragma -mark PrivateMethod
 - (CGFloat)caculateTwoPointDistance:(CGPoint)firstPoint secondPoint:(CGPoint)secondPoint {
     CGFloat XDistance = (firstPoint.x - secondPoint.x) * (firstPoint.x - secondPoint.x);
     CGFloat YDistance = (firstPoint.y - secondPoint.y) * (firstPoint.y - secondPoint.y);
@@ -164,10 +192,6 @@
     if (self.superview) {
         [self removeFromSuperview];
     }
-}
-
-- (IBAction)rotateButtonTapped:(id)sender {
-    
 }
 
 - (IBAction)scaleButtonTapped:(id)sender {
